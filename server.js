1e4e06728f2d76d620fa511ab9d0cc4884e19672
NGUYEN THI QUYNH ANH <127226698+winndy112@users.noreply.github.com>
2024-03-17 16:49:01 +0700

    sidebar

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const app = express();
 const port = 11000;
-const host = "192.168.102.104";
-
+const host = "192.168.102.105";
 
 const { readFileSync, writeFileSync } = require("fs");
 
-// app.use(express.static('public'));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-app.set("view engine", "ejs")
+// Serve static files from the 'interface' folder
+app.use(express.static('interface'));
+
+// Serve CSS files from the 'css' folder inside the 'interface' folder
+app.use('/css', express.static(__dirname + '/css'));
+
+// Set the view engine to EJS
+app.set("view engine", "ejs");
 
 // Return 
 app.get("/", (req, res) => {
-    res.render("intro")
-})
-
-
-
+    res.render("intro");
+});
 
 app.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
-// Don't write any code below this line
